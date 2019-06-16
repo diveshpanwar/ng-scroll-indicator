92f9e1b38ac002fd64e2e40805322fb3363ebee7
diveshpanwar <divesh.panwar@gmail.com>
2019-06-16 20:32:40 +0530

    Added Provision to Change Color

--- projects/scroller/src/lib/scroller.component.ts
+++ projects/scroller/src/lib/scroller.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 'scroll-indicator',
-  templateUrl: './scroll-indicator.component.html',
+  template: `<div class="scroll-container">
+                <div class="scroll" id="scroller" [ngStyle]="{'background-color': color}">
+                </div>
+              </div>
+            `,
   styleUrls: ['./scroll-indicator.component.css'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
   preserveWhitespaces: false
 })
 export class ScrollerComponent implements OnInit {
-
+  @Input() color: string;
   constructor() { }
 
   ngOnInit() {
+    this._checkInputs();
     window.onscroll = () => { this.handleScrollIndicator(); };
+  }
+
+  ngOnChanges() {
+    this._checkInputs();
   }
 
   handleScrollIndicator() {
@@ -21,5 +30,9 @@ export class ScrollerComponent implements OnInit {
     const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     const scrolled = (winScroll / height) * 100;
     document.getElementById("scroller").style.width = scrolled + "%";
+  }
+
+  private _checkInputs() {
+    this.color = this.color ? this.color : '#0ad0e6';
   }
 }
--- projects/scroller/src/lib/scroller.module.ts
+++ projects/scroller/src/lib/scroller.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ScrollerComponent } from './scroller.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [ScrollerComponent],
   imports: [
+    CommonModule
   ],
   exports: [ScrollerComponent]
 })
